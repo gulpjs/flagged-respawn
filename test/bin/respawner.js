52d44fb2f3ed845213fc5a1aa6bd493df5779319
sttk <sttk.xslet@gmail.com>
2017-12-07 23:00:00 +0900

    Scaffold: Normalize repository

--- test/bin/respawner.js
+++ test/bin/respawner.js
@@ -1,17 +1,24 @@
 #!/usr/bin/env node
 
 const flaggedRespawn = require('../../');
+const v8flags = require('v8flags');
 
 // get a list of all possible v8 flags for the running version of node
-const v8flags = require('v8flags').fetch();
+v8flags(function(err, flags) {
+  if (err) {
+    console.error(err);
+    return;
+  }
 
-flaggedRespawn(v8flags, process.argv, function (ready, child) {
-  if (ready) {
-    console.log('Running!');
-  } else {
-    console.log('Special flags found, respawning.');
-  }
-  if (child.pid !== process.pid) {
-    console.log('Respawned to PID:', child.pid);
-  }
+  flaggedRespawn(flags, process.argv, function (ready, child) {
+    if (ready) {
+      console.log('Running!');
+    } else {
+      console.log('Special flags found, respawning.');
+    }
+    if (child.pid !== process.pid) {
+      console.log('Respawned to PID:', child.pid);
+    }
+  });
 });
+
