8980b58048860f2f785eaa2854d9d63a2e9a934b
sttk <sttk.xslet@gmail.com>
2017-12-10 19:24:55 +0900

    Fix: Ensure correct exit codes are returned on failure

---
+++ test/bin/callback-params.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+
+const flaggedRespawn = require('../../');
+const v8flags = require('v8flags');
+
+// get a list of all possible v8 flags for the running version of node
+v8flags(function(err, flags) {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  flaggedRespawn(flags, process.argv, function (ready, child) {
+    var result = {
+      ready: ready,
+      child_pid: child.pid,
+      process_pid: process.pid,
+    };
+    console.log(JSON.stringify(result));
+  });
+});
+
